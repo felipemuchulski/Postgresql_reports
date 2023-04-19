a8ee8201eba02ff78216789caa5ae2f55affac4e
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2023-04-19 15:47:24 -0300

    Update select_reports.sql

--- select_reports.sql
+++ select_reports.sql
@@ -1,5 +1,5 @@
    
-SELECT 'psql  -U postgres -d ' || datname || '  -c  "SELECT ''******************** D a t a d a b a s e  ''||current_database() ||'' ***********************''  " -t  >> /var/lib/postgresql/scripts/stats/reports.txt'
+SELECT 'psql  -U postgres -d ' || datname || '  -c  "SELECT ''******************** D a t a d a b a s e  ''|| current_database() ||'' ***********************''  " -t  >> /var/lib/postgresql/scripts/stats/reports.txt'
    FROM pg_stat_database
   WHERE datname IS NOT NULL
    and datname <> 'template0' and datname <> 'template1'
